a38db99262966377d6437f675f29984a61aa7c30
Erin Grooms <eringrooms3746@gmail.com>
2019-03-07 11:41:23 -0600

    add methods in java files

--- PriorityQueueHospital.java
+++ PriorityQueueHospital.java
@@ -1,4 +1,28 @@
 
-public class PriorityQueueHospital {
+public class PriorityQueueHospital<PatientType> extends Hospital<PatientType>{
+	
+	public void addPatient(PatientType patient) {
+		
+	}
+	
+	public PatientType nextPatient() {
+		return null;
+	}
+	
+	public PatientType treatNextPatient() {
+		return null;
+	}
+	
+	public int numPatients() {
+		return 0;
+	}
+	
+	public String hospitalType() {
+		return null;
+	}
+	
+	public String allPatientInfo() {
+		return null;
+	}
 
 }
--- QueueHospital.java
+++ QueueHospital.java
@@ -1,4 +1,31 @@
 
-public class QueueHospital {
-
+public class QueueHospital<PatientType> extends Hospital<PatientType> {
+	
+	public void addPatient(PatientType patient) {
+		
+	}
+	
+	public PatientType nextPatient() {
+		return null;
+	}
+	
+	public PatientType treatNextPatient() {
+		return null;
+	}
+	
+	public int numPatients() {
+		return 0;
+	}
+	
+	public String hospitalType() {
+		return null;
+	}
+	
+	public String allPatientInfo() {
+		return null;
+	}
+	
 }
+
+
+
