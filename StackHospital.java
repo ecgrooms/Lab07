0af40995f0e0281ce240943c6fd7c199c4e01d67
Erin Grooms <eringrooms3746@gmail.com>
2019-03-07 11:23:27 -0600

    add files for lab 7

---
+++ StackHospital.java
@@ -0,0 +1,35 @@
+
+public class StackHospital<PatientType> extends Hospital<PatientType>{
+	
+	public int numPatients() {
+		return 0;
+	}
+	
+	public String hospitalType() {
+		return null;
+	}
+	
+	public String allPatientInfo() {
+		return null;
+	}
+
+	@Override
+	public void addPatient(PatientType patient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PatientType nextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PatientType treatNextPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
