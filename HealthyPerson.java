c6a14c6dc6095e983434910dc5abb2a23b8acc4e
Erin Grooms <eringrooms3746@gmail.com>
2019-03-07 11:34:32 -0600

    add methods in HealthyPerson.java

--- HealthyPerson.java
+++ HealthyPerson.java
@@ -1,4 +1,17 @@
 
-public class HealthyPerson {
+public class HealthyPerson extends Person{
+	
+	public HealthyPerson(String name, int age, String reason) {
+		super(name, age);
+	}
+	
+	protected int compareToImpl(Person p) {
+		return 0;
+	}
+	
+	public String toString() {
+		return null;
+		
+	}
 
 }
