c449793ee4aa8d5a4e942620ff3e751347d3f107
Erin Grooms <eringrooms3746@gmail.com>
2019-03-07 11:30:39 -0600

    add files for lab 7

---
+++ SickPerson.java
@@ -0,0 +1,16 @@
+
+public class SickPerson extends Person {
+	
+	public SickPerson(String name, int age, int serverity) {
+		super(name, age);
+	}
+	
+	public int compareToImpl(Person) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+
+}
