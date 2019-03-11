4469574120c8b8d9302de40822f41fe9b20ac5e2
Erin Grooms <eringrooms3746@gmail.com>
2019-03-11 12:39:28 -0500

    modified HealthyPerson.java and SickPerson.java

--- HealthyPerson.java
+++ HealthyPerson.java
@@ -1,16 +1,38 @@
 
 public class HealthyPerson extends Person{
 	
+	private String reason;
+	
 	public HealthyPerson(String name, int age, String reason) {
 		super(name, age);
+		
+		this.reason = reason;
 	}
 	
 	protected int compareToImpl(Person p) {
-		return 0;
+		
+		if(p instanceof HealthyPerson) {
+			
+			HealthyPerson otherHealthyPerson = (HealthyPerson) p;
+			
+			if( otherHealthyPerson.reason.compareTo(this.reason) < 0 ) {
+				return 1;
+			}
+			else if( otherHealthyPerson.reason.compareTo(this.reason) > 0 ){
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+		
 	}
 	
 	public String toString() {
-		return null;
+		return String.format("%s In for %s", super.toString(), reason);
 		
 	}
 
--- SickPerson.java
+++ SickPerson.java
@@ -1,16 +1,39 @@
 
 public class SickPerson extends Person {
 	
+	private int serverity;
+	
 	public SickPerson(String name, int age, int serverity) {
 		super(name, age);
+		
+		this.serverity = serverity;
+		
 	}
 	
 	protected int compareToImpl(Person p) {
-		return 0;
+		
+		if(p instanceof SickPerson) {
+			
+			SickPerson otherSickPerson = (SickPerson) p;
+			
+			if(otherSickPerson.serverity > this.serverity) {
+				return 1;
+			}
+			else if(otherSickPerson.serverity < this.serverity){
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+		
 	}
 	
 	public String toString() {
-		return null;
+		return String.format("%s Severity level %d", super.toString(), serverity);
 	}
 
 }
